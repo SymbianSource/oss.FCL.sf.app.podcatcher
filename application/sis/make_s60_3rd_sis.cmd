7fbaf753601147461cd8547ecedb4705cd01cfe3
Sebastian Brannstrom <sebastianb@symbian.org>
2010-07-22 16:50:56 +0100

    Updated sis creation script
    --HG--
    branch : 3rded

--- application/sis/make_s60_3rd_sis.cmd
+++ application/sis/make_s60_3rd_sis.cmd
@@ -1,6 +1,6 @@
 del *.sis
 makesis -dc:\symbian\S60_3rd_FP1 podcatcher_udeb.pkg
-signsis podcatcher_udeb.sis podcatcher_udeb_signed.sis podcast.cer podcast.key
+signsis podcatcher_udeb.sis podcatcher_3rd_udeb_signed.sis podcast.cer podcast.key
 
 makesis -dc:\symbian\S60_3rd_FP1 podcatcher_urel.pkg
 signsis podcatcher_urel.sis podcatcher_urel_signed.sis podcast.cer podcast.key
