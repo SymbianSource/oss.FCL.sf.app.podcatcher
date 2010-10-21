367c65829333a2f2fb62f8684cdb4a00f82526b5
Sebastian Brannstrom <sebastianb@symbian.org>
2010-10-21 18:52:18 +0100

    Adding new toolbar icons
    --HG--
    branch : RCL_3

--- application/sis/make_s3_sis.cmd
+++ application/sis/make_s3_sis.cmd
@@ -1,6 +1,3 @@
 del *.sis
-makesis -d\ podcatcher_udeb.pkg
+makesis -d\symbian\pdk3.0.2\ podcatcher_udeb.pkg
 signsis podcatcher_udeb.sis podcatcher_udeb_signed.sis podcast.cer podcast.key
-
-makesis -d\ podcatcher_urel.pkg
-signsis podcatcher_urel.sis podcatcher_urel_signed.sis podcast.cer podcast.key
